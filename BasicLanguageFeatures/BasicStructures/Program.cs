f7f1029cada8ad26ece1607452f23246d3d7173d
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-10-14 17:19:46 +0200

    Hello World updated

--- BasicLanguageFeatures/BasicStructures/Program.cs
+++ BasicLanguageFeatures/BasicStructures/Program.cs
@@ -6,7 +6,7 @@ namespace BasicStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again");
         }
     }
 }
