bbca8ed9d288db2a3c109ff0e85be69c0a7443b3
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-10-30 18:12:30 +0100

    Program 10 finished

--- BasicLanguageFeatures/BasicStructures/Program.cs
+++ BasicLanguageFeatures/BasicStructures/Program.cs
@@ -1,11 +1,97 @@
 ﻿using System;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
 namespace BasicStructures
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            Program10c();
+        }
+
+        private static void Program10c()
+        {
+            var list = new List<int>();
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Enter a number (no {i + 1})");
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            var sum = 0;
+
+            foreach (var element in list)
+                sum += element;
+
+            var average = 1.0 * sum / list.Count;
+
+            Console.WriteLine($"Sum is {sum} and average is {average}");
+        }
+
+        private static void Program10b()
+        {
+            var array = new int[4];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Enter a number (no {i + 1})");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            var sum = 0;
+
+            foreach (var element in array)
+                sum += element;
+
+            var average = 1.0 * sum / array.Length;
+
+            Console.WriteLine($"Sum is {sum} and average is {average}");
+        }
+
+        private static void Program10a()
+        {
+            Console.WriteLine("Enter the first number");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number");
+            var c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the fourth number");
+            var d = Convert.ToInt32(Console.ReadLine());
+
+            var sum = a + b + c + d;
+            var average = sum / 4.0;
+
+            Console.WriteLine($"Sum is {sum} and average is {average}");
+        }
+
+        private static void Program6()
+        {
+            Console.WriteLine("Input the radius of the circle : ");
+            var radius = Convert.ToDouble(Console.ReadLine());
+
+            var perimeter = 2 * Math.PI * radius;
+            var area = Math.PI * radius * radius;
+
+            Console.WriteLine($"Perimeter of Circle : {perimeter} and area is {area}");
+            Console.ReadKey();
+        }
+
+        private static void Program9()
+        {
+            Console.Write("Please write the temperature in Celsius: ");
+            var cel = Convert.ToDouble(Console.ReadLine());
+            var fahr = cel * 1.8 + 32;
+            var kel = cel + 273.15;
+
+            Console.WriteLine($"Temperature in Fahrenheit: {fahr} F\nTemperature in Kelvins: {kel} K");
+        }
+
+        private static void Program8()
         {
             Console.WriteLine("Enter the length of the first side");
             var a = Convert.ToDouble(Console.ReadLine());
