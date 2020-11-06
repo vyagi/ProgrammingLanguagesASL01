c6c22872cae6694b1847057eb45c48902478ca5d
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-11-06 19:53:52 +0100

    Car - first implementation

---
+++ ObjectOrientedProgrammingFundamentals/BasicClasses/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    public class Person
+    {
+        private string _lastName;
+        private string _firstName;
+        private int _age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+                throw new ArgumentException("First name should not be empty");
+
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException("Last name should not be empty");
+
+            if (age < 0)
+                throw new ArgumentException("Age should not be negative");
+
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+
+        public string FullName => $"{_firstName} {_lastName}";
+
+        public bool IsAdult => _age >= 18;
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Last name should not be empty");
+
+                _lastName = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("First name should not be empty");
+
+                _firstName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Age should not be negative");
+
+                _age = value;
+            }
+        }
+    }
+}
--- ObjectOrientedProgrammingFundamentals/BasicClasses/Program.cs
+++ ObjectOrientedProgrammingFundamentals/BasicClasses/Program.cs
@@ -4,7 +4,79 @@ namespace BasicClasses
 {
     class Program
     {
+        public class Car
+        {
+            private readonly string _make;
+            private readonly string _color;
+            private readonly int _yearOfProduction;
+            private readonly int _fuelCapacity;
+            private readonly int _fuelUsagePer100Km;
+            private int _kilometerCounter;
+            private int _fuelLevel;
+
+            public string Make => _make;
+            public string Color => _color;
+            public int YearOfProduction => _yearOfProduction;
+            public int KilometerCounter => _kilometerCounter;
+            public int FuelLevel => _fuelLevel;
+
+            public Car(string make, string color, int yearOfProduction, int fuelCapacity, int fuelUsagePer100Km)
+            {
+                _make = make;
+                _color = color;
+                _yearOfProduction = yearOfProduction;
+                _fuelCapacity = fuelCapacity;
+                _fuelUsagePer100Km = fuelUsagePer100Km;
+            }
+
+            public void Tank(int howManyLiters)
+            {
+                if (howManyLiters < 0 )
+                    throw new ArgumentException("You need to provide positive value");
+
+                if (_fuelLevel + howManyLiters > _fuelCapacity) 
+                    _fuelLevel = _fuelCapacity;
+                else
+                    _fuelLevel += howManyLiters;
+            }
+
+            public void Drive(int distance)
+            {
+                if (distance < 0)
+                    throw new ArgumentException("You need to provide positive value");
+
+                var range = _fuelLevel * 100 / _fuelUsagePer100Km;  
+
+                if (distance > range)   
+                {
+                    //if you have 3 litres in you car, and you want to drive 100 km, and your car uses 6 l / 100km
+                    _kilometerCounter += range;
+                    _fuelLevel = 0;
+                }
+                else
+                {
+                    _kilometerCounter += distance;
+                    _fuelLevel -= distance * _fuelUsagePer100Km / 100;
+                }
+            }
+        }
+
+
         static void Main(string[] args)
+        {
+             var car1 = new Car("Ford", "White", 2019, 60, 6);
+             
+             car1.Tank(300);
+             car1.Drive(100);
+
+             car1.Drive(50);
+
+             Console.WriteLine($"{car1.Make} produced in {car1.YearOfProduction} is {car1.Color}");
+             Console.WriteLine($"It drove {car1.KilometerCounter} and currently has {car1.FuelLevel} liters of fuel");
+             
+        }
+
+        private static void PersonTests()
         {
             var person1 = new Person("Marcin", "Jagieła", 19);
             var person2 = new Person("Donald", "Trump", 70);
@@ -19,68 +91,5 @@ namespace BasicClasses
             person3.LastName = "Wałęsa";
             Console.WriteLine(person3.LastName);
         }
-    }
-    
-    public class Person
-    {
-        public Person(string firstName, string lastName, int age)
-        {
-            if (string.IsNullOrWhiteSpace(firstName))
-                throw new ArgumentException("First name should not be empty");
-
-            if (string.IsNullOrWhiteSpace(lastName))
-                throw new ArgumentException("Last name should not be empty");
-
-            if (age < 0)
-                throw new ArgumentException("Age should not be negative");
-
-            _firstName = firstName;
-            _lastName = lastName;
-            _age = age;
-        }
-
-        public string FullName => $"{_firstName} {_lastName}";
-
-        public bool IsAdult => _age >= 18;
-
-        public string LastName
-        {
-            get => _lastName;
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentException("Last name should not be empty");
-
-                _lastName = value;
-            }
-        }
-
-        public string FirstName
-        {
-            get => _firstName;
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentException("First name should not be empty");
-
-                _firstName = value;
-            }
-        }
-
-        public int Age
-        {
-            get => _age;
-            set
-            {
-                if (value < 0)
-                    throw new ArgumentException("Age should not be negative");
-
-                _age = value;
-            }
-        }
-
-        private string _lastName;
-        private string _firstName;
-        private int _age;
     }
 }
