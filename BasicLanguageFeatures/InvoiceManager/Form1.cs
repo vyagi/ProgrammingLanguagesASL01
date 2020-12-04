1a4aa6d6cbec4dee87784eff122c2a45b319d05a
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-12-04 19:30:45 +0100

    Invoice manager - work in progress + invoice file

--- BasicLanguageFeatures/InvoiceManager/Form1.cs
+++ BasicLanguageFeatures/InvoiceManager/Form1.cs
@@ -16,5 +16,10 @@ namespace InvoiceManager
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = !checkBox1.Checked;
+        }
     }
 }
